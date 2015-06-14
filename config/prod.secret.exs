71ea241aab73e313a8da4343bb3ea21422cc59d9
Arthur Colle <hello@arthurcolle.com>
2015-06-14 14:22:12 -0400

    tryfix2

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -6,13 +6,13 @@ use Mix.Config
 config :hello_phoenix, HelloPhoenix.Endpoint,
   secret_key_base: "RsieqYq5+ENldo51eOxftSHZUMsRUII3fBmpx/b5hc6Y/vdABg3rqt2WMis+dnpJ"
 
-# # Configure your database
-# config :hello_phoenix, HelloPhoenix.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   username: "postgres",
-#   password: "postgres",
-#   database: "hello_phoenix_prod",
-#   size: 20 # The amount of database connections in the pool
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "hello_phoenix_prod",
+  size: 20 # The amount of database connections in the pool
 
 defmodule Heroku do
   def database_config(uri) do
