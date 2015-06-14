a47aa49b5146b847268f13b76c6d60ab16bdf737
Arthur Colle <hello@arthurcolle.com>
2015-06-14 14:35:11 -0400

    Fix secrets

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,13 +3,11 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :hello_phoenix, HelloPhoenix.Endpoint,
-  secret_key_base: "RsieqYq5+ENldo51eOxftSHZUMsRUII3fBmpx/b5hc6Y/vdABg3rqt2WMis+dnpJ"
+config :heroku_test_app, HelloPhoenix.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :heroku_test_app, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "hello_phoenix_prod",
+  url: System.get_env("DATABASE_URL"),
   size: 20 # The amount of database connections in the pool
