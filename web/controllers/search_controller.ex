a2f1a09241609b4327a4f69e00387a1714508595
Arthur Colle <hello@arthurcolle.com>
2015-06-23 19:05:20 -0400

    Search works

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -1,7 +1,29 @@
 defmodule Trophus.SearchController do
+	require ErlasticSearch
 	use Trophus.Web, :controller
 	plug :action
   def get_nearest(conn, params) do
+  	query = params["query"]
+  	IO.inspect query
+  	ss = ErlasticSearch.erls_params(host: "colle.space")
+  	result = :erlastic_search.search(ss, "trophus", "name:#{query}*")  
+  	result2 = :erlastic_search.search(ss, "trophus", "description:#{query}*")
+  	{:ok, res} = result
+  	IO.inspect res
+  	{:ok, res2} = result2
+  	IO.inspect res2
+  	rex = Dict.merge res, res2
+  	IO.inspect rex
+  	# IO.inspect res["hits"]
+  	names = 
+  	rex["hits"]
+  	|> Enum.map fn(hit) -> hit end
+  	IO.puts "names are..."
+  	{:ok, {"hits", gett}} = names |> Enum.fetch 0
+  	IO.inspect gett
+  	item_names = gett
+  	|> Enum.map fn(item) -> ["dish", item["_source"]["user_id"], item["_source"]["dish_id"], item["_source"]["name"]] end
+
   	curr_id = conn.private.plug_session["current_user"]
   	users = Trophus.Repo.all(Trophus.User) |> Enum.filter fn(x) -> x.id != curr_id end
 
@@ -42,10 +64,10 @@ defmodule Trophus.SearchController do
     	   other_user = Trophus.Repo.get(Trophus.User, num)
     	   oname = other_user.name
     	   id = other_user.id
-    	   [id, oname, dst]
+    	   ["user", id, oname, dst]
     	 end
-    IO.inspect expanded
-    json conn, %{results: expanded}
+    IO.inspect  (expanded ++ (item_names |> Enum.map fn(["dish", uid, did, dnm]) -> ["dish", uid, did, dnm] end))
+    json conn, %{results: (expanded ++ (item_names |> Enum.map fn(["dish", uid, did, dnm]) -> ["dish", uid, did, dnm] end))}
   end
 
   def display do
