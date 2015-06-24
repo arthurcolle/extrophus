b35e6f0ca5e3cc551e7f76d22abfaa18c72cdc18
Arthur Colle <hello@arthurcolle.com>
2015-06-23 22:30:32 -0400

    Revert

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -7,94 +7,68 @@ defmodule Trophus.SearchController do
   	IO.inspect query
   	ss = ErlasticSearch.erls_params(host: System.get_env("ELASTIC_URL"))
   	
-  	name_results = :erlastic_search.search(ss, "trophus", "name:#{query}*")  
-    desc_results = :erlastic_search.search(ss, "trophus", "description:#{query}*")  
-  	#result2 = :erlastic_search.search(ss, "trophus", "description:#{query}*")
-  	{:ok, res1} = name_results
-    {:ok, res2} = desc_results
-    # IO.puts "--------------------------------"
-    # IO.puts "BINGO IS STARTING"
-    # hits1 = res1 |> Enum.filter fn({x,y}) -> if x == "hits" do y end end
-    # hits2 = res2 |> Enum.filter fn({x,y}) -> if x == "hits" do y end end
-    # #[{"total",_a},{"max_score",_b},{"hits",bingo}]
-
-    # [{"hits", %{"hits" => rest1}}] = hits1
-    # [{"hits", %{"hits" => rest2}}] = hits2
-    # item_names = rest1 |>
-    # Enum.map fn(rest_item) ->
-    #   Poison.encode! rest_item end
-    # IO.inspect item_names
-
-    # item_names2 = rest2 |>
-    # Enum.map fn(rest_item) ->
-    #   Poison.encode! rest_item end
-    # IO.inspect item_names2
-
-    # #bingo |> Enum.map fn(list_elt) -> IO.inspect list_elt end
-    # IO.puts "BINGO IS OVER"
-    # IO.puts "--------------------------------"
-
-  	# IO.inspect res
-  	# {:ok, res2} = result2
-  	# IO.inspect res2
-  	# rex = Map.merge res, res2
-  	# IO.inspect rex
+  	result = :erlastic_search.search(ss, "trophus", "name:#{query}*")  
+  	result2 = :erlastic_search.search(ss, "trophus", "description:#{query}*")
+  	{:ok, res} = result
+  	IO.inspect res
+  	{:ok, res2} = result2
+  	IO.inspect res2
+  	rex = Map.merge res, res2
+  	IO.inspect rex
   	# IO.inspect res["hits"]
-  	# names = 
-  	# res["hits"]
-  	# |> Enum.map fn(hit) -> hit end
-  	# IO.puts "names are..."
-  	# {:ok, {"hits", gett}} = names |> Enum.fetch 0
-  	# IO.inspect gett
-  	# item_names = gett
-  	# |> Enum.map fn(item) -> ["dish", item["_source"]["user_id"], item["_source"]["dish_id"], item["_source"]["name"]] end
+  	names = 
+  	rex["hits"]
+  	|> Enum.map fn(hit) -> hit end
+  	IO.puts "names are..."
+  	{:ok, {"hits", gett}} = names |> Enum.fetch 0
+  	IO.inspect gett
+  	item_names = gett
+  	|> Enum.map fn(item) -> ["dish", item["_source"]["user_id"], item["_source"]["dish_id"], item["_source"]["name"]] end
 
-  	# curr_id = conn.private.plug_session["current_user"]
-  	# users = Trophus.Repo.all(Trophus.User) |> Enum.filter fn(x) -> x.id != curr_id end
+  	curr_id = conn.private.plug_session["current_user"]
+  	users = Trophus.Repo.all(Trophus.User) |> Enum.filter fn(x) -> x.id != curr_id end
 
-  	# current_user = Trophus.Repo.get(Trophus.User, curr_id)
-  	# c = current_user
-  	# current_user_geo = %{
-  	# 	latitude: c.latitude, 
-  	# 	longitude: c.longitude
-  	# }
-   #  tuple_list = 
-   #  (users |>
-   #  Enum.map fn(x) ->
-   #  	x_geo = %{
-   #  		latitude: x.latitude, 
-   #  		longitude: x.longitude
-   #  	}
+  	current_user = Trophus.Repo.get(Trophus.User, curr_id)
+  	c = current_user
+  	current_user_geo = %{
+  		latitude: c.latitude, 
+  		longitude: c.longitude
+  	}
+    tuple_list = 
+    (users |>
+    Enum.map fn(x) ->
+    	x_geo = %{
+    		latitude: x.latitude, 
+    		longitude: x.longitude
+    	}
 
-	  # 	# data = %{	
-	  # 	# 	current_user: c.id, 
-	  # 	# 	other_user: x.id, 
-	  # 	# 	distance: Compare.distance(x_geo, current_user_geo)
-	  # 	# }
-   #  	# data
+	  	# data = %{	
+	  	# 	current_user: c.id, 
+	  	# 	other_user: x.id, 
+	  	# 	distance: Compare.distance(x_geo, current_user_geo)
+	  	# }
+    	# data
 
-   #  	dx = {x.id, Compare.distance(x_geo, current_user_geo)}   	
-   #  	dx
-   #  end)
-   #  better_list =
-   #  (tuple_list 
-   #  |> List.keysort 1)
-   #  |> Enum.map fn(x) -> Tuple.to_list x end
+    	dx = {x.id, Compare.distance(x_geo, current_user_geo)}   	
+    	dx
+    end)
+    better_list =
+    (tuple_list 
+    |> List.keysort 1)
+    |> Enum.map fn(x) -> Tuple.to_list x end
 
-   #  expanded = 
-   #  better_list
-   #  |> Enum.map fn(elt) ->
-   #  	   num = hd elt
-   #  	   dst = List.last elt
-   #  	   other_user = Trophus.Repo.get(Trophus.User, num)
-   #  	   oname = other_user.name
-   #  	   id = other_user.id
-   #  	   ["user", id, oname, dst]
-   #  	 end
-   #  IO.inspect  (expanded ++ (item_names |> Enum.map fn(["dish", uid, did, dnm]) -> ["dish", uid, did, dnm] end))
-   #  json conn, %{results: (expanded ++ (item_names |> Enum.map fn(["dish", uid, did, dnm]) -> ["dish", uid, did, dnm] end))}
-    {:ok, nr} = name_results
-    json conn, %{results: Poison.encode! nr}
+    expanded = 
+    better_list
+    |> Enum.map fn(elt) ->
+    	   num = hd elt
+    	   dst = List.last elt
+    	   other_user = Trophus.Repo.get(Trophus.User, num)
+    	   oname = other_user.name
+    	   id = other_user.id
+    	   ["user", id, oname, dst]
+    	 end
+    IO.inspect  (expanded ++ (item_names |> Enum.map fn(["dish", uid, did, dnm]) -> ["dish", uid, did, dnm] end))
+    json conn, %{results: (expanded ++ (item_names |> Enum.map fn(["dish", uid, did, dnm]) -> ["dish", uid, did, dnm] end))}
   end
 
   def display do
