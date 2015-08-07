2adc6b78f8a6790ab588cca95157dc4ce580d805
Arthur Colle <hello@arthurcolle.com>
2015-08-07 00:13:43 -0400

    Refactoring code to follow elixir style guide

--- web/controllers/cart_controller.ex
+++ web/controllers/cart_controller.ex
@@ -1,6 +1,5 @@
 defmodule Trophus.CartController do
   use Trophus.Web, :controller
-  # import Trophus.Helpers
 
   alias Trophus.Repo
   alias Trophus.Order
@@ -10,17 +9,14 @@ defmodule Trophus.CartController do
 
   def show(conn, params) do
     user = Repo.get(User, params["user_id"])
-    items = (
+    items =
       Repo.get(Order, user.current_order) 
       |> Repo.preload :order_items
-    )
-    
-    order_items_list = (
+
+    order_items_list =
       items.order_items 
       |> Repo.preload :dish
-    )
-    # IO.puts "ORDER ITEMS LIST"
-    # IO.inspect order_items_list
+
     quantity_tuple_list = []
     {:ok, agent} = Agent.start_link(fn -> [] end)
     uniq_items = Enum.uniq order_items_list, fn(oi) -> oi.dish_id end
@@ -35,43 +31,50 @@ defmodule Trophus.CartController do
   def get_current_order(conn, %{"id" => user_id}) do
     current_user = Trophus.Repo.get(Trophus.User, user_id)
     current_order = Trophus.Repo.get(Trophus.Order, current_user.current_order)
-    # IO.puts "This is the current_order"
-    # IO.inspect current_order
     render(conn, "cart.html", current_order: current_order)
   end
 
   def add_to_cart(conn, params) do
     dish = Repo.get(Dish, params["dish_id"])
     current_user = Repo.get(User, params["current_user"])
-    if current_user.current_order == nil do
-      current_order_changeset = Order.changeset(%Order{}, %{"user_id" => current_user.id})
+    if current_user.current_order |> is_nil do
+      current_user_dict = %{"user_id" => current_user.id}
+      current_order_changeset = 
+        current_user_dict
+        |> Order.changeset %Order{}
       if current_order_changeset.valid? do
         current_order_response = Repo.insert! current_order_changeset
-        # IO.puts "Current order response is..."
         current_order_id = current_order_response.id
-        user_current_order_changeset = User.changeset(current_user, %{current_order: current_order_response.id})
+        current_order_dict = %{current_order: current_order_response.id}
+        
+        user_current_order_changeset = 
+          current_user 
+          |> User.changeset current_order_dict
+
         if user_current_order_changeset.valid? do
-          ucoc_response = Repo.update! user_current_order_changeset
-          IO.puts "Current order USER response is..."
-          IO.inspect ucoc_response
+          Repo.update! user_current_order_changeset
         end
       end
     else 
-      # IO.puts "GOT HERE"
       current_order = Repo.get(Order, current_user.current_order)
       order_item_changeset_params = %{"dish_id" => dish.id, "order_id" => current_order.id}
       order_item_changeset = OrderItem.changeset(%OrderItem{}, order_item_changeset_params)
-      # IO.puts "DO I EXIST?"
-      # IO.inspect order_item_changeset
       if order_item_changeset.valid? do
-        oic = Repo.insert! order_item_changeset
-        # IO.puts "Adding to cart CHANGESET RESPONSE"
-        # IO.inspect oic
+        Repo.insert! order_item_changeset
       end
-      order_items = (current_order |> Repo.preload :order_items)
-      items = (order_items.order_items |> Repo.preload :dish)
-      IO.inspect items
-      item_price_list = (items |> Enum.map fn(item) -> item.dish.price end)
+
+      order_items = 
+        current_order 
+        |> Repo.preload :order_items)
+      
+      items = 
+        order_items.order_items 
+        |> Repo.preload :dish
+      
+      item_price_list =
+        items 
+        |> Enum.map fn(item) -> item.dish.price end
+      
       current_total = Enum.reduce(item_price_list, 0, &+/2)
       json conn, %{params: params, current_order_total: current_total}
     end
@@ -80,10 +83,18 @@ defmodule Trophus.CartController do
   def get_current_order_balance(conn, params) do
     current_user = Repo.get(User, params["current_user"])
     current_order = Repo.get(Order, current_user.current_order)
-    order_items = (current_order |> Repo.preload :order_items)
-    items = (order_items.order_items |> Repo.preload :dish)
-    IO.inspect items
-    item_price_list = (items |> Enum.map fn(item) -> item.dish.price end)
+    order_items = 
+      current_order 
+      |> Repo.preload :order_items
+
+    items = 
+      order_items.order_items 
+      |> Repo.preload :dish
+
+    item_price_list = 
+      items 
+      |> Enum.map fn(item) -> item.dish.price end
+
     current_total = Enum.reduce(item_price_list, 0, &+/2)
     json conn, %{params: params, current_order_total: current_total}
   end
--- web/controllers/order_controller.ex
+++ web/controllers/order_controller.ex
@@ -15,13 +15,11 @@ defmodule Trophus.OrderController do
 
   def order_dish(conn, params) do
     dish_id = String.to_integer(params["dish_id"])
-    # Trophus.Repo.all(Trophus.Dish) |> Trophus.Repo.preload [:user]
     ordered_dish =
-		Repo.get(Dish, dish_id)
-		|> Repo.preload [:user]
+      Repo.get(Dish, dish_id)
+      |> Repo.preload [:user]
 
     seller = Repo.get(User, ordered_dish.user.id)
-    # ordering_user = Trophus.Repo.get(Trophus.User, conn.private.plug_session["current_user"])
     buyer_id = String.to_integer(params["buyer_id"])
     buyer = Repo.get(User, ordered_dish.user.id)
 
@@ -29,16 +27,12 @@ defmodule Trophus.OrderController do
 
     buyer = Repo.get(User, buyer_id)
     changeset = params
-    # IO.inspect "THE PARAMS FOR ORDER DISH ARE..."
-    # IO.inspect params
 
     customer_id = buyer.customer_id
     connect_id = seller.connect_id
     HTTPotion.start
     content_type = "application/x-www-form-urlencoded"
-    auth = "Bearer sk_test_aqQo51A1cGQEk09BCaCGmkYZ"
     auth = "Bearer #{System.get_env("STRIPE_SECRET_KEY")}"
-    # auth = "Bearer sk_live_0qBm4bSeORZikHqoLFwRzRC3"
     stripe_charges_url = "https://api.stripe.com/v1/charges"
     headers = ["Content-type": content_type, "Authorization": auth]
 
@@ -53,27 +47,12 @@ defmodule Trophus.OrderController do
     application_fee = "application_fee="<>Integer.to_string(app_fee)<>"&"
     customer_to_charge = "customer="<>customer_id
 
-    assembled_payload_content =
+    payload_content =
       dest<>amount<>curr<>application_fee<>customer_to_charge
-
-    payload_content = assembled_payload_content
 
     payload = [body: payload_content, headers: headers]
     response = HTTPotion.post stripe_charges_url, payload
-    # IO.inspect "STRIPE RESPONSE!!!!"
     {:ok, decoded_response} = Poison.decode response.body
-    IO.inspect decoded_response
-    IO.inspect seller.name
-    IO.inspect ordered_dish.name
     render(conn, "charge.html", response: decoded_response, seller: seller, item: ordered_dish)
-
-    # if changeset.valid? do
-    #   Repo.insert(changeset)
-    #   conn
-    #   |> put_flash(:info, "Order made successfully.")
-    #   |> redirect(to: order_path(conn, :show))
-    # else
-    #   render(conn, "show.html", changeset: changeset)
-    # end
   end
 end
